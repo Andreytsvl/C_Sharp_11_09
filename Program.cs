078aab453623c98837359d82eca97d7830df18d0
unknown <andreytsvl15@gmail.com>
2024-09-11 11:24:45 +0300

    sem_6_1

--- Program.cs
+++ Program.cs
@@ -17,20 +17,91 @@
 // Console.Write(str[0]);
 
 
-string str = Console.ReadLine();
-string result = GetLettersFromString(str);
-Console.WriteLine(result);
+// string str = Console.ReadLine();
+// string result = GetLettersFromString(str);
+// Console.WriteLine(result);
 
-string GetLettersFromString(string s)
+// string GetLettersFromString(string s)
+// {
+//     string letters = "";
+//     foreach (char e in s)
+//     {
+//         if (char.IsAsciiLetter(e) == true)
+//         {
+//             letters = letters + e;
+//         }
+//     }
+//     return letters;
+// }
+
+// task 39 Напишите программу, которая перевернёт одномерный массив.
+// Последний элемент будет на первом месте, а первый на последнем, и т. д.
+
+using System;
+using static System.Console;
+
+// Clear();
+// Write("Введите размер массива:");
+// int n = int.Parse(ReadLine());
+// int[] array1 = GetArray(n, 1, 10);
+// WriteLine(string.Join(" ", array1));
+
+// int[] array2 = ReverseArray1(array1);
+// WriteLine(string.Join(" ", array2));
+
+// ReverseArray2(array1);
+// WriteLine(string.Join(" ", array1));
+
+// int[] GetArray(int size, int min, int max)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = new Random().Next(min, max + 1);
+//     }
+//     return result;
+// }
+
+// int[] ReverseArray1(int[] inArray)
+// {
+//     int[] result = new int[inArray.Length];
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = inArray[inArray.Length - 1 - i];
+//     }
+//     return result;
+// }
+
+// void ReverseArray2(int[] inArray)
+// {
+//     for (int i = 0; i < inArray.Length/2; i++)
+//     {
+//         int k = inArray[i];
+//         inArray[i]= inArray[inArray.Length-1-i];
+//         inArray[inArray.Length-1-i] = k;
+
+//     }
+// }
+
+// task 40 Программа принимает длинны трёх сторон треугольника
+// и проверяет, может ли существовать такой треугольник
+Clear();
+Write("Введите стороны треугольника через пробел: ");
+string[] nums = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+if(IsTriangle(int.Parse(nums[0]),int.Parse(nums[1]),int.Parse(nums[2])))
 {
-    string letters = "";
-    foreach (char e in s)
-    {
-        if (char.IsAsciiLetter(e) == true)
-        {
-            letters = letters + e;
-        }
-    }
-    return letters;
+    WriteLine("Треугольник существует.");
+}
+else
+{
+    WriteLine("Треугольник не существует.");
 }
 
+
+bool IsTriangle(int a, int b, int c)
+{
+    return ((a+b>c)&(a+c>b)&(b+c>a));
+}
+
+// task 42 Напишите программу, которая преобразует десятичное
+// число в двоичное.
