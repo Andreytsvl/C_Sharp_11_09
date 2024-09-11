5d947163cdd9e80efc733a8e624aaf2215a900fd
unknown <andreytsvl15@gmail.com>
2024-09-11 09:30:02 +0300

    lect 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿
+// int [,] matrix = new int[3,5]; 
+// Random rnd = new Random();
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j] = rnd.Next(1,1000);
+//         Console.Write($"{matrix[i,j]} ");
+
+//         // matrix[i,j] = rnd.Next(1,11);
+//     }
+//     Console.WriteLine();
+// }
+
+// string str = "Hello";
+// Console.Write(str[0]);
+
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
